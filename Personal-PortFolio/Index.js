3e5551cfc50b40679e1594ccd059a136b26e4459
Rohit Kumar <rohit.verrma13@gmail.com>
2023-03-24 22:48:57 +0530

    Add files via upload

---
+++ Personal-PortFolio/Index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#main-nav a").click(function(){
+        $("section").removeClass("show");
+        var target = $(this).attr("href");
+        $(target).addClass("show");
+    })
+
+});
